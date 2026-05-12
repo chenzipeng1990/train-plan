76c08bb07e8be2946a43a7870253fb7281e44642
chenzipeng <chenzipeng1990@gmail.com>
2026-05-12 11:07:49 +0800

    build(router): 切换路由模式为hash模式
    将原本的HTML5历史模式切换为哈希模式，适配非服务端渲染的部署环境，避免直接访问子路由出现404问题

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * 路由配置
  */
 
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import WeightChartView from '../views/WeightChartView.vue';
 
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
